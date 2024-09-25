ce8a022cc3d6ad2795809b0f835e976a85c986cf
hamandy2000 <minji9306@gmail.com>
2024-09-25 16:14:52 +0900

    일반로그인 init

--- src/main/java/com/app/bewodeurim/mapper/member/MemberMapper.java
+++ src/main/java/com/app/bewodeurim/mapper/member/MemberMapper.java
@@ -1,4 +1,10 @@
 package com.app.bewodeurim.mapper.member;
 
+import com.app.bewodeurim.domain.member.MemberVO;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
 public interface MemberMapper {
+    //회원정보 입력
+    public void insertMemberInfo(MemberVO memberVO);
 }
--- src/main/resources/sql/member/member.sql
+++ src/main/resources/sql/member/member.sql
@@ -1,44 +1,23 @@
 /* 회원 테이블 */
+DROP TABLE TBL_MEMBER;
+
 CREATE TABLE TBL_MEMBER (
     ID NUMBER CONSTRAINT PK_MEMBER PRIMARY KEY,  /* 회원 테이블의 기본 키 */
     MEMBER_NAME VARCHAR2(100) NOT NULL,  /* 회원 이름 */
-    MEMBER_EMAIL VARCHAR2(100) NOT NULL UNIQUE,  /* 회원 이메일 (고유) */
+    MEMBER_EMAIL VARCHAR2(100) NOT NULL ,  /* 회원 이메일 (고유) */
     MEMBER_ADDRESS_CODE VARCHAR2(255) NOT NULL, /*우편번호*/
     MEMBER_ADDRESS_BASIC VARCHAR2(255) NOT NULL,  /* 기본 주소 */
     MEMBER_ADDRESS_DETAIL VARCHAR2(255) NOT NULL, /*상세 주소*/
-    MEMBER_PHONE_NUMBER VARCHAR2(15) NOT NULL,  /* 회원 전화번호 */
-    MEMBER_STATUS VARCHAR2(15) NOT NULL,  /* 회원 상태 (예: 활동중, 탈퇴함) */
+    MEMBER_PHONE_NUMBER VARCHAR2(255) NOT NULL,  /* 회원 전화번호 */
+    MEMBER_STATUS VARCHAR2(15) DEFAULT '활동중',  /* 회원 상태 (예: 활동중, 탈퇴함) */
     MEMBER_PASSWORD VARCHAR2(100) NOT NULL, /* 회원 비밀번호 */
+    MEMBER_TYPE VARCHAR2(255) DEFAULT '일반회원',/* 회원 분류(예: 일반회원,기사회원) */
     CREATED_DATE DATE DEFAULT SYSDATE, /* 회원 가입 날짜 */
     UPDATED_DATE DATE DEFAULT SYSDATE
 );
+
 CREATE SEQUENCE SEQ_MEMBER;
 
 SELECT * FROM TBL_MEMBER;
 
-INSERT INTO TBL_MEMBER (
-    ID,
-    MEMBER_NAME,
-    MEMBER_EMAIL,
-    MEMBER_ADDRESS_CODE,
-    MEMBER_ADDRESS_BASIC,
-    MEMBER_ADDRESS_DETAIL,
-    MEMBER_PHONE_NUMBER,
-    MEMBER_STATUS,
-    MEMBER_PASSWORD,
-    CREATED_DATE,
-    UPDATED_DATE
-) VALUES (
-             SEQ_MEMBER.NEXTVAL,  -- 자동 증가 시퀀스를 사용하는 경우
-             'John Doe',  -- 회원 이름
-             'john.doe@example.com',  -- 회원 이메일
-             '12345',  -- 우편번호
-             '123 Main St',  -- 기본 주소
-             'Apt 456',  -- 상세 주소
-             '010-1234-5678',  -- 전화번호
-             '활동중',  -- 회원 상태
-             'password123',  -- 비밀번호
-             SYSDATE,  -- 가입 날짜
-             SYSDATE  -- 업데이트 날짜
-         );
 
