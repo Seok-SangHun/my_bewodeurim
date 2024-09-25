8ab74a446380bb37de6a0e8e80e5cc88aec1b178
kimgame102 <koreait2337@gmail.com>
2024-09-25 19:22:40 +0900

    init

--- src/main/java/com/app/bewodeurim/mapper/inquiry/InquiryMapper.java
+++ src/main/java/com/app/bewodeurim/mapper/inquiry/InquiryMapper.java
@@ -1,10 +1,14 @@
 package com.app.bewodeurim.mapper.inquiry;
 
+
 import com.app.bewodeurim.domain.inquiry.InquiryVO;
 import org.apache.ibatis.annotations.Mapper;
+
+
 
 @Mapper
 public interface InquiryMapper {
     //  1:1 문의 작성
     public void insert(InquiryVO inquiryVO);
+
 }
--- src/main/java/com/app/bewodeurim/repository/inquiry/InquiryDAO.java
+++ src/main/java/com/app/bewodeurim/repository/inquiry/InquiryDAO.java
@@ -15,5 +15,4 @@ public class InquiryDAO {
         inquiryMapper.insert(inquiryVO);
     }
 
-
 }
