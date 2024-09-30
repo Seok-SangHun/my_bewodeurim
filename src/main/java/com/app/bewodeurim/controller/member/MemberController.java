601254cd98cc332af1dba39fcdbc3d0088318124
hamandy2000 <minji9306@gmail.com>
2024-09-30 17:20:43 +0900

    로그인 업데이트

--- src/main/java/com/app/bewodeurim/controller/member/MemberController.java
+++ src/main/java/com/app/bewodeurim/controller/member/MemberController.java
@@ -28,33 +28,36 @@ import java.util.Optional;
 public class MemberController {
     private final MemberService memberService;
 
-    @GetMapping("login")
+    @GetMapping("/login/web_login")
 //    @RequestParam(required = false)
 //    전달받은 데이터가 null일 경우 required의 default값이 true이기 때문에,
 //    NPE가 발생할 수 있다. 이를 필수가 아닌 선택으로(null 허용) 변경하고 싶다면,
 //    required 설정을 false로 지정한다.
     public void goToLoginForm(@RequestParam(required = false) Boolean status, MemberDTO memberDTO, HttpServletRequest request, Model model){
-//        log.info("{}", status);
+        log.info("status: {}", status);
 
 //        쿠키 조회
         Cookie[] cookies = request.getCookies();
 
-        for(int i = 0; i < cookies.length; i++){
-            log.info(cookies[i].getName());
-//            save라는 key가 있다면,
-            if(cookies[i].getName().equals("save")){
-//                해당 value를 화면으로 보낸다.
-                model.addAttribute("save", cookies[i].getValue());
+        if (cookies != null){
+            for(int i = 0; i < cookies.length; i++){
+                log.info(cookies[i].getName());
+    //            save라는 key가 있다면,
+                if(cookies[i].getName().equals("save")){
+    //                해당 value를 화면으로 보낸다.
+                    model.addAttribute("save", cookies[i].getValue());
+
+                }
+                if(cookies[i].getName().equals("memberEmail")){
+                    memberDTO.setMemberEmail(cookies[i].getValue());
+                }
 
             }
-            if(cookies[i].getName().equals("memberEmail")){
-                memberDTO.setMemberEmail(cookies[i].getValue());
-            }
-
         }
+
     }
 
-    @PostMapping("login")
+    @PostMapping("/login/web_login")
 //    HttpSession
 //    서버의 Session영역을 관리해주는 객체이다.
 //    Spring이 해당 객체를 주입해준다.
@@ -69,25 +72,17 @@ public class MemberController {
 //                            log.info("로그인 실패");
 //                        });
 
-
+//        // 로그인 시도
         Optional<MemberVO> foundMember = memberService.login(memberDTO.toVO());
-//        null인지 검사할 때
-//        if(memberService.login(memberDTO.toVO()).isPresent()){
 //
-//        }
-
-//        null이 아닐 때에만 실행
-//        foundMember.ifPresent((member) -> {
-//            session.setAttribute("memberId", member.getId());
-//        });
-
-//        null이 아니면 단일 객체 리턴, null이면 예외 발생
+////        null이 아니면 단일 객체 리턴, null이면 예외 발생
         MemberVO memberVO = foundMember.orElseThrow(() -> {throw new LoginFailException("(" + LocalTime.now() + ")로그인 실패");});
+//
+////        id만 담아놓으면 사용할 때마다 SELECT 쿼리를 발생시켜야 한다(싫어!)
+////        session.setAttribute("memberId", memberVO.getId());
+////        전체 정보를 담아놓기 때문에 쿼리를 따로 발생시킬 필요 없다(좋아!)
+//        session.setAttribute("member", memberVO);
 
-//        id만 담아놓으면 사용할 때마다 SELECT 쿼리를 발생시켜야 한다(싫어!)
-//        session.setAttribute("memberId", memberVO.getId());
-//        전체 정보를 담아놓기 때문에 쿼리를 따로 발생시킬 필요 없다(좋아!)
-        session.setAttribute("member", memberVO);
 
 //        화면에서 아이디 저장을 선택했다면 null이 아니다.
         if(save != null){
@@ -114,8 +109,8 @@ public class MemberController {
             response.addCookie(memberEmailCookie);
         }
 
-
-        return new RedirectView( "/post/list");
+        log.info("로그인 성공: {}", memberVO);
+        return new RedirectView( "/main");
     }
 
     //    로그 아웃
@@ -123,5 +118,12 @@ public class MemberController {
     public RedirectView logout(HttpSession session){
         session.invalidate();
         return new RedirectView("/member/login");
+    }
+
+    //    메인페이지 이동
+    @GetMapping("/main")
+    public void  goToWebMain(HttpSession session){
+        session.invalidate();
+//        return new RedirectView("/main");
     }
 }
--- src/main/java/com/app/bewodeurim/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/bewodeurim/exception/GlobalExceptionHandler.java
@@ -5,12 +5,13 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.view.RedirectView;
 
-@ControllerAdvice(basePackages = "com.app.threetier.controller.member")
+@ControllerAdvice(basePackages = "com.app.bewodeurim.controller.member")
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(LoginFailException.class)
     protected RedirectView handleLoginFailException(LoginFailException e) {
         log.error(e.getMessage());
-        return new RedirectView("/member/login?status=false");
+        return new RedirectView("/login/web_login?status=false");
+
     }
 }
