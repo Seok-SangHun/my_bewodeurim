969192f468ab080b42619e7f01bd1be3b111cc19
hamandy2000 <minji9306@gmail.com>
2024-10-01 07:18:22 +0900

    모바일 로그인 업데이트

--- src/main/java/com/app/bewodeurim/exception/GlobalExceptionHandler.java
+++ src/main/java/com/app/bewodeurim/exception/GlobalExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.app.bewodeurim.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,9 +10,10 @@ import org.springframework.web.servlet.view.RedirectView;
 @Slf4j
 public class GlobalExceptionHandler {
     @ExceptionHandler(LoginFailException.class)
-    protected RedirectView handleLoginFailException(LoginFailException e) {
+    protected RedirectView handleLoginFailException(LoginFailException e , HttpServletRequest request) {
+        String currentPath = request.getRequestURI();
         log.error(e.getMessage());
-        return new RedirectView("/login/web_login?status=false");
+        return new RedirectView(currentPath + "?status=false");
 
     }
 }
